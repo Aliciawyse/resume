0be5a4ab6cf0e9e9fd7134ca98319550403a586d
Alicia <Alicia.barrett89@gmail.com>
2019-08-18 16:06:10 -0400

    Don't need collapsed nav bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -30,7 +30,7 @@ export class Sidebar extends Component {
             {config.firstName} {config.lastName}
           </span>
         </a>
-        <button
+        {/* <button
           className="navbar-toggler"
           type="button"
           data-toggle="collapse"
@@ -40,7 +40,7 @@ export class Sidebar extends Component {
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
-        </button>
+        </button> */}
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <Scrollspy
             items={tabs.map(s => s.href)}
